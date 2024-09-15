fec8a7a293965868dc0326605239ef7960f2d6fc
Rajesh Kumar Ramadas <rajesh.kumar.r@continental-corporation.com>
2024-09-15 17:20:20 +0530

    updated colab code
    updated colab code

--- demo/train_yolo8/yolo8_custom_train.py
+++ demo/train_yolo8/yolo8_custom_train.py
@@ -4,6 +4,6 @@ from ultralytics import YOLO
 # Load a model
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 # Use the model
-model.train(data="/content/config.yaml", epochs=20)  # train the model
+model.train(data="/content/config.yaml", epochs=50)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
-results = model("/content/converted_png/images/test/d4c52577df97e69f39917c7f1dda8adc512dbf17_2000x2000.png")  # predict on an image
+results = model("/content/converted_png3/images/test/3.png")  # predict on an image
